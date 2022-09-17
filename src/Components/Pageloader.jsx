bb44654d448980ce44c98b3fee948f9d3cbe058d
mJunaid15 <junaidjjd44@gmail.com>
2022-09-17 15:07:20 +0500

    Updated address endpoint

--- src/Components/Pageloader.jsx
+++ src/Components/Pageloader.jsx
@@ -4,20 +4,6 @@ import React from "react";
 const Pageloader = () => {
   return (
     <>
-      {/* <div class="border bg-black border-blue-300 shadow rounded-md p-4">
-        <div class="animate-pulse  ">
-          <div class="flex-1 space-y-6 py-1">
-            <div class="h-2 bg-slate-200 rounded"></div>
-            <div class="space-y-3">
-              <div class="grid grid-cols-12 gap-4">
-                <div class="h-2 bg-slate-200 rounded "></div>
-                <div class="h-2 bg-slate-200 rounded  "></div>
-              </div>
-              <div class="h-2 bg-slate-200 rounded"></div>
-            </div>
-          </div>
-        </div>
-      </div> */}
 
       <TableRow
         sx={{
